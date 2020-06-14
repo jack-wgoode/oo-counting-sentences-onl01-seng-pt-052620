d2214fc881956f2cc4c423f9a162ea806c0a678a
Jack Goode <jack.w.goode@icloud.com>
2020-06-14 18:17:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    @array = self.split(/[.?!]/)
+    @array = self.split(/[.?!]/).reject {|string| string.empty?}
     return @array.length
     
     
